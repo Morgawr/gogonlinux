8f98b52ec670ecfe89f243348f7815b0ae71eed7
Mateusz Kowalczyk <mk440@bath.ac.uk>
2012-11-25 20:07:33 +0000

    Disable falsely reported pylint errors due to unresolved library type
    Signed-off-by: Morgawr <morgawr@gmail.com>

--- gog_utils/gol_connection.py
+++ gog_utils/gol_connection.py
@@ -12,16 +12,16 @@ BETA_GAMES = "/available-beta"
 def obtain_available_games():
     """Returns JSON list of all available games."""
     resp = requests.get(url=(WEBSITE_URL + AVAILABLE_GAMES))
-    return json.loads(resp.text)
+    return json.loads(resp.text) #pylint: disable=E1103
 
 def obtain_beta_available_games():
     """Obtains JSON list of all available beta games."""
     resp = requests.get(url=(WEBSITE_URL + BETA_GAMES))
-    return json.loads(resp.text)
+    return json.loads(resp.text) #pylint: disable=E1103
 
 def download_script(target, url):
     """Function to download data from url to target."""
     reqs = requests.get(url)
     with open(target, "w+") as file_handle:
-        file_handle.write(reqs.content)
+        file_handle.write(reqs.content) #pylint: disable=E1103
     os.chmod(target, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)

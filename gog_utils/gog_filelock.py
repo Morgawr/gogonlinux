e38e5aab9800ada48f997edd5c3724cf22216ca8
Morgawr <morgawr@gmail.com>
2012-11-26 15:39:17 +0100

    fixed more pylint stuff with gog_filelock

--- gog_utils/gog_filelock.py
+++ gog_utils/gog_filelock.py
@@ -1,6 +1,8 @@
-# This filelock module has been taken and modified from
-# http://www.evanfosmark.com/2009/01/cross-platform-file-locking-support-in-python/ 
-# Thanks to Evan Fosmark for this code
+"""
+This filelock module has been taken and modified from
+www.evanfosmark.com/2009/01/cross-platform-file-locking-support-in-python/ 
+Thanks to Evan Fosmark for this code.
+"""
 
 
 import os
@@ -8,17 +10,21 @@ import time
 import errno
  
 class FileLockException(Exception):
+    """ Class for FileLock exception type. """
     pass
  
 class FileLock(object):
-    """ A file locking mechanism that has context-manager support so 
-        you can use it in a with statement. This should be relatively cross
-        compatible as it doesn't rely on msvcrt or fcntl for the locking.
+    """
+    A file locking mechanism that has context-manager support so 
+    you can use it in a with statement. This should be relatively cross
+    compatible as it doesn't rely on msvcrt or fcntl for the locking.
     """
  
     def __init__(self, file_name, path, timeout=10, delay=.05):
-        """ Prepare the file locker. Specify the file to lock, the path and optionally
-            the maximum timeout and the delay between each attempt to lock.
+        """ 
+        Prepare the file locker. Specify the file to lock, the path 
+        and optionally the maximum timeout and the delay between 
+        each attempt to lock.
         """
         self.is_locked = False
         if not os.path.exists(path):
@@ -30,18 +36,20 @@ class FileLock(object):
  
  
     def acquire(self):
-        """ Acquire the lock, if possible. If the lock is in use, it check again
-            every `wait` seconds. It does this until it either gets the lock or
-            exceeds `timeout` number of seconds, in which case it throws 
-            an exception.
+        """ 
+        Acquire the lock, if possible. If the lock is in use, it check again
+        every `wait` seconds. It does this until it either gets the lock or
+        exceeds `timeout` number of seconds, in which case it throws 
+        an exception.
         """
         start_time = time.time()
         while True:
             try:
-                self.fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
-                break;
-            except OSError as e:
-                if e.errno != errno.EEXIST:
+                self.file_handle = os.open(self.lockfile, 
+                                           os.O_CREAT|os.O_EXCL|os.O_RDWR)
+                break
+            except OSError as err:
+                if err.errno != errno.EEXIST:
                     raise 
                 if (time.time() - start_time) >= self.timeout:
                     raise FileLockException("Timeout occured.")
@@ -50,19 +58,21 @@ class FileLock(object):
  
  
     def release(self):
-        """ Get rid of the lock by deleting the lockfile. 
-            When working in a `with` statement, this gets automatically 
-            called at the end.
+        """ 
+        Get rid of the lock by deleting the lockfile. 
+        When working in a `with` statement, this gets automatically 
+        called at the end.
         """
         if self.is_locked:
-            os.close(self.fd)
+            os.close(self.file_handle)
             os.unlink(self.lockfile)
             self.is_locked = False
  
  
     def __enter__(self):
-        """ Activated when used in the with statement. 
-            Should automatically acquire a lock to be used in the with block.
+        """ 
+        Activated when used in the with statement. 
+        Should automatically acquire a lock to be used in the with block.
         """
         if not self.is_locked:
             self.acquire()
@@ -70,15 +80,17 @@ class FileLock(object):
  
  
     def __exit__(self, type, value, traceback):
-        """ Activated at the end of the with statement.
-            It automatically releases the lock if it isn't locked.
+        """
+        Activated at the end of the with statement.
+        It automatically releases the lock if it isn't locked.
         """
         if self.is_locked:
             self.release()
  
  
     def __del__(self):
-        """ Make sure that the FileLock instance doesn't leave a lockfile
-            lying around.
+        """ 
+        Make sure that the FileLock instance doesn't leave a 
+        lockfile lying around.
         """
         self.release()
